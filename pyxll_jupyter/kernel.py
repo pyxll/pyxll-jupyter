pyxll/pyxll-jupyter

commit c48ec6c3d0acafd7ff0af372717edabc3b3ebd8f

Jupyter url doesn't always have query parameters
If a password is set then the URL doesn't include a token as a query
parameter.

Fixes #29

pyxll_jupyter/kernel.py
@@ -504,7 +504,7 @@ def launch_jupyter(initial_path=None,
 
         raise RuntimeError("Timed-out waiting for the Jupyter notebook URL.")
 
-    root, params = url.split("?", 1)
+    root, params = url.split("?", 1) if "?" in url else url, ""
     params = params.split("&")
     params.extend(_subcommand_query_params[subcommand])
 

commit 51d667dbd5b94fa268b950b17a6bf69387a4c1a1

Fix bug introduced in c48ec6c3d0acafd7ff0af372717edabc3b3ebd8f

pyxll_jupyter/kernel.py
@@ -504,7 +504,7 @@ def launch_jupyter(initial_path=None,
 
         raise RuntimeError("Timed-out waiting for the Jupyter notebook URL.")
 
-    root, params = url.split("?", 1) if "?" in url else url, ""
+    root, params = url.split("?", 1) if "?" in url else (url, "")
     params = params.split("&")
     params.extend(_subcommand_query_params[subcommand])
 